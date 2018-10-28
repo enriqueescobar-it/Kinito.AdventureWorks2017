ce5ca56fc1460d342a0aec7f214da5c00fd891ad
escenr70 <escenr70@LGI18Q3044>
2018-10-27 22:26:10 -0400

    4 rpooject 3 nugets

--- Configurations/Properties/AssemblyInfo.cs
+++ Configurations/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Interfaces")]
+[assembly: AssemblyTitle("Configurations")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Interfaces")]
+[assembly: AssemblyProduct("Configurations")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0075a9c2-c960-4f0d-a356-deba9c4e0e26")]
+[assembly: Guid("ba0a9ed8-7611-423b-bafe-0973f10fba1c")]
 
 // Version information for an assembly consists of the following four values:
 //
